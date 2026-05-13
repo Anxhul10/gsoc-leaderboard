e99caccdb117442bf772ec835d9dfa2a9c86ede7
Anshul Ekka <anxhul10@gmail.com>
2026-05-14 01:47:55 +0530

    chore: integration next.js and MIUI

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,11 @@
+import TextField from "@mui/material/TextField";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <div>app logo</div>
+      <TextField id="outlined-basic" label="Search" variant="outlined" />
+      <div>github link</div>
+    </div>
+  );
 }
