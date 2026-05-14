fa7cd666429564171f929395ef81c760171f6ce9
Anshul Ekka <anxhul10@gmail.com>
2026-05-14 13:49:07 +0530

    fix: formating

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import { default as Navbar } from "@/components/Navbar";
 import "./globals.css";
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,23 +4,26 @@ import { redirect } from "next/navigation";
 
 export default function Navbar() {
   return (
-    
     <div className="Navbar">
       <div className="Navbar-left">
-            <Image
-                src="/icon0.svg"
-                alt="GitHub"
-                loading="eager"
-                width={50}
-                height={50}
-                onClick={() => redirect("/")}
-                />
-      </div>
-      <TextField id="outlined-basic" label="Search" variant="outlined" size="small"
-          sx={{
-            width: 1/2
-          }}
+        <Image
+          src="/icon0.svg"
+          alt="GitHub"
+          loading="eager"
+          width={50}
+          height={50}
+          onClick={() => redirect("/")}
         />
+      </div>
+      <TextField
+        id="outlined-basic"
+        label="Search"
+        variant="outlined"
+        size="small"
+        sx={{
+          width: 1 / 2,
+        }}
+      />
       <div className="Navbar-right">
         <Image
           src="/icon1.svg"
@@ -28,7 +31,9 @@ export default function Navbar() {
           loading="eager"
           width={30}
           height={30}
-          onClick={() => redirect("https://github.com/Anxhul10/gsoc-leaderboard")}
+          onClick={() =>
+            redirect("https://github.com/Anxhul10/gsoc-leaderboard")
+          }
         />
       </div>
     </div>
