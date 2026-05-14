fcf195337f7e842feaf7b2681edcd06c2f825915
Anshul Ekka <anxhul10@gmail.com>
2026-05-14 13:48:43 +0530

    feat: add navbar and icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+'use client';
 import { default as Navbar } from "@/components/Navbar";
+import "./globals.css";
 
 export default function Page() {
   return <Navbar />;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,36 @@
 import TextField from "@mui/material/TextField";
+import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default function Navbar() {
   return (
-    <div>
-      <div>app logo</div>
-      <TextField id="outlined-basic" label="Search" variant="outlined" />
-      <div>github link</div>
+    
+    <div className="Navbar">
+      <div className="Navbar-left">
+            <Image
+                src="/icon0.svg"
+                alt="GitHub"
+                loading="eager"
+                width={50}
+                height={50}
+                onClick={() => redirect("/")}
+                />
+      </div>
+      <TextField id="outlined-basic" label="Search" variant="outlined" size="small"
+          sx={{
+            width: 1/2
+          }}
+        />
+      <div className="Navbar-right">
+        <Image
+          src="/icon1.svg"
+          alt="GitHub"
+          loading="eager"
+          width={30}
+          height={30}
+          onClick={() => redirect("https://github.com/Anxhul10/gsoc-leaderboard")}
+        />
+      </div>
     </div>
   );
 }
