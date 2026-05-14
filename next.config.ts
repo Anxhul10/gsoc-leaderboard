055dffd739ea5da9b23abea5194075a0f7b6efd7
Anshul Ekka <anxhul10@gmail.com>
2026-05-14 21:25:29 +0530

    add base path

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
+  basePath: "/gsoc-leaderboard",
 };
 
 export default nextConfig;
