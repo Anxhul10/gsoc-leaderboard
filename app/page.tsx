8842255991a1588e7addb0841d456e7fe5477156
Anshul Ekka <anxhul10@gmail.com>
2026-06-08 00:29:26 +0530

    add orgs from data

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,68 @@
 "use client";
-import { default as Navbar } from "@/components/Navbar";
 import "./globals.css";
 
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import CardActionArea from "@mui/material/CardActionArea";
+import data from "@/data/2026.json";
+
 export default function Page() {
-  return <Navbar />;
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))",
+        gap: "20px",
+        padding: "20px",
+      }}
+    >
+      {data.map((org) => (
+        <Org
+          key={org.slug}
+          name={org.name}
+          image={org.logo_url}
+          description={org.description}
+        />
+      ))}
+    </div>
+  );
+}
+
+function Org({
+  name,
+  image,
+  description,
+}: {
+  name: string;
+  image: string;
+  description: string;
+}) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          image={image}
+          alt={name}
+          sx={{
+            height: 180,
+            objectFit: "contain",
+            bgcolor: "background.default",
+            p: 2,
+          }}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5">
+            {name}
+          </Typography>
+
+          <Typography variant="body2" sx={{ color: "text.secondary" }}>
+            {description.slice(0, 120)}...
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
 }
