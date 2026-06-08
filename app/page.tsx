5a990bcf15aa910b5a940bb92525929deaeeeb47
Anshul Ekka <anxhul10@gmail.com>
2026-06-08 08:01:32 +0530

    refactor: remove org function

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
 "use client";
 import "./globals.css";
-
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import CardActionArea from "@mui/material/CardActionArea";
 import data from "@/data/2026.json";
+import Org from "@/components/Org";
 
 export default function Page() {
   return (
@@ -27,42 +22,5 @@ export default function Page() {
         />
       ))}
     </div>
-  );
-}
-
-function Org({
-  name,
-  image,
-  description,
-}: {
-  name: string;
-  image: string;
-  description: string;
-}) {
-  return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          image={image}
-          alt={name}
-          sx={{
-            height: 180,
-            objectFit: "contain",
-            bgcolor: "background.default",
-            p: 2,
-          }}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5">
-            {name}
-          </Typography>
-
-          <Typography variant="body2" sx={{ color: "text.secondary" }}>
-            {description.slice(0, 120)}...
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
   );
 }
