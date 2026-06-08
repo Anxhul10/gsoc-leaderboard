d902657cdb892e411c185d3341770fc88760bcdc
Anshul Ekka <anxhul10@gmail.com>
2026-06-08 09:15:01 +0530

    feat: add search & normalize

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,3 +1,8 @@
+import data from "@/data/2026.json";
+import Org from "@/components/Org";
+
+const normalize = (str: string) => str.toLowerCase().replace(/[^a-z0-9]/g, "");
+
 export default async function Page({
   searchParams,
 }: {
@@ -5,9 +10,40 @@ export default async function Page({
 }) {
   const { q = "" } = await searchParams;
 
+  const filteredData = data.filter((org) => {
+    const search = normalize(q);
+
+    return (
+      normalize(org.name).includes(search) ||
+      normalize(org.tagline).includes(search) ||
+      org.tech_tags.some((tag) => normalize(tag).includes(search)) ||
+      org.topic_tags.some((tag) => normalize(tag).includes(search))
+    );
+  });
+
   return (
     <>
-      <h1>Search results for {q}</h1>
+      <h2>
+        <i>Search results for "{q}"</i>
+      </h2>
+
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))",
+          gap: "20px",
+          padding: "20px",
+        }}
+      >
+        {filteredData.map((org) => (
+          <Org
+            key={org.slug}
+            name={org.name}
+            image={org.logo_url}
+            description={org.description}
+          />
+        ))}
+      </div>
     </>
   );
 }
